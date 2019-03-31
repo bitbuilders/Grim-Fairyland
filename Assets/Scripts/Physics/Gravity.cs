bc0c00375b25223fb711c0cd299b5d00cfc230e0
Colin Misbach <colin@misbach.com>
2019-03-30 19:19:01 -0600

    :construction_worker: Chain and Ball Stuff Comin' Together

--- Assets/Scripts/Physics/Gravity.cs
+++ Assets/Scripts/Physics/Gravity.cs
@@ -10,6 +10,8 @@ public class Gravity : MonoBehaviour
     [SerializeField] [Range(0.01f, 100.0f)] float m_downwardForce = 1.0f;
       [Tooltip("Will only add gravity if the rigid velocity is greater than this value (less than the negative of the value for downward force)")]
     [SerializeField] [Range(0.0001f, 0.1f)] float m_benchmark = 0.001f;
+      [Tooltip("A multiplier for how fast the object will fall (0.0 = will not affect, 1.0 = will double every frame)")]
+    [SerializeField] [Range(0.0f, 1.0f)] float m_gravityMultiplier = 0.5f;
 
     Rigidbody m_rigidBody;
 
@@ -20,13 +22,15 @@ public class Gravity : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (m_rigidBody.velocity.y > m_benchmark)
+        float y = m_rigidBody.velocity.y;
+        if (y > m_benchmark)
         {
             m_rigidBody.velocity += Physics.gravity * (m_upwardDampening - 1.0f) * Time.deltaTime;
         }
-        else if (m_rigidBody.velocity.y < -m_benchmark)
+        else if (y < -m_benchmark)
         {
             m_rigidBody.velocity += Physics.gravity * (m_downwardForce - 1.0f) * Time.deltaTime;
+            m_rigidBody.velocity += y * Vector3.up * m_gravityMultiplier;
         }
     }
 }

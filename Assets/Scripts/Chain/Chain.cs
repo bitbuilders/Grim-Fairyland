e84e76446e12e47b55f8564584a3d8205beff61e
Colin Misbach <colin@misbach.com>
2019-04-13 21:24:47 -0600

    Chain follows player and has max distance

---
+++ Assets/Scripts/Chain/Chain.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chain : MonoBehaviour
+{
+    public struct ChainTransform
+    {
+        public Vector3 Position;
+        public Quaternion Rotation;
+    }
+
+    [SerializeField] GameObject m_chainHead = null;
+    [SerializeField] GameObject m_chainTail = null;
+    [SerializeField] [Range(0.0f, 25.0f)] float m_maxChainLength = 5.0f;
+
+    public void SetTransforms(ChainTransform tailTransform, ChainTransform headTransform)
+    {
+        SetTailTransform(tailTransform);
+        SetHeadTransform(headTransform);
+    }
+
+    public void SetHeadTransform(ChainTransform transform)
+    {
+        if (m_chainHead)
+        {
+            m_chainHead.transform.position = transform.Position;
+            m_chainHead.transform.rotation = transform.Rotation;
+            Vector3 dir = m_chainHead.transform.position - m_chainTail.transform.position;
+            float dist = dir.magnitude;
+            if (dist > m_maxChainLength)
+            {
+                m_chainHead.transform.position = dir.normalized * m_maxChainLength + m_chainTail.transform.position;
+            }
+        }
+    }
+
+    public void SetTailTransform(ChainTransform transform)
+    {
+        if (m_chainTail)
+        {
+            m_chainTail.transform.position = transform.Position;
+            m_chainTail.transform.rotation = transform.Rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/Pop/PopChain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopChain : MonoBehaviour
+{
+    [SerializeField] Roll m_roll = null;
+    [SerializeField] Chain m_chain = null;
+    
+    
+    void Update()
+    {
+        Vector3 tail = GetLocalBack();
+        Vector3 head = m_roll.transform.position;
+        Vector3 dir = tail - head;
+        head += dir.normalized * (m_roll.transform.localScale.x * 0.7f);
+
+        Chain.ChainTransform t = new Chain.ChainTransform() { Position = tail, Rotation = Quaternion.identity };
+        Chain.ChainTransform h = new Chain.ChainTransform() { Position = head, Rotation = Quaternion.identity };
+
+        m_chain.SetTransforms(t, h);
+    }
+    
+    Vector3 GetLocalBack()
+    {
+        return -transform.forward + transform.position;
+    }
+}
